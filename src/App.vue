0046c41e623466bb8886b70d7fd2646b8ff301a4
zuvy <zuvanich@gmail.com>
2025-05-18 11:49:14 -0500

    working version of passing props through the parent-child chain.

--- src/App.vue
+++ src/App.vue
@@ -2,52 +2,21 @@
   <div>
     <app-header/><br>
     <div class="container">
-      <UserProfile
-        
-        :userLastname = "data.lastname"
-        :userAge = "data.age"
-        :userParents = "data.parents"
-        @update-lastname="data.lastname = $event"
-        @say-hello="alertHello"
-        :updateAge="updateAge"
-      />
-      <!-- the @say-hello listens for the results of the alertHello function
-           but is triggered by the say-hello emitted event from App.vue-->
-      <button @click="updateName">Update name</button>
+      <Cars :cars="cars"/>
     </div>
   </div>
 </template>
 
 <script setup>
-  import {ref} from 'vue';
-  // this is the local way of importing a component -- uses the name of the emported file.
-  // see main.js for global
-  //import Footer from './components/header_footer/Footer.vue';
-
-  import UserProfile from './components/User/Profile.vue';
+  // using reactive since we are working with objects (ref has more overhead)
   import { reactive } from 'vue';
-  const data = reactive({
-    lastname: 'Beethoven',
-    name: 'Rocket',
-    age: '34',
-    parents: {
-      father: 'Mario',
-      mother: 'Gretchen'
-    }
-  })
-
-  const updateName = () => {
-    data.name = "Golden child"
-  }
-
-  const alertHello = () => {
-    alert("Hello");
-  }
-
-  const updateAge = (value) => {
-    data.age = value;
-  }
-
+  import Cars from './components/Cars/index.vue';
+  // creating the content to inject into the children (Cars --> list) see the reactive bind variable :cars="cars" in the container div
+  const cars = reactive([
+    { model: 'Dino', brand: 'Ferrari'},
+    { model: '911', brand: 'Posrche'},
+    { model: 'Tipo', brand: 'Fiat'}
+  ])
 </script>
 
 <!-- style tags are global no matter what component you put it in unless you scope it --> 
---
+++ src/components/Cars/list.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- imported into Cars component -->
+  <div>
+    <ul v-for="car in cars" :key="car.model">
+      <li>
+        <span>{{ car.model }}</span>: {{ car.brand }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  // props defined originally in App.vue imported into the Cars component and finally made available
+  // to display here in the list template.
+  // passing props from parent to child gets unwieldy when the parent child chain gets longer.
+  const props = defineProps(['cars']);
+</script>
