d872615187e64a345ca70eede09f7647ab2978a9
zuvy <zuvanich@gmail.com>
2025-05-23 17:36:20 -0500

    Local custom directive.

--- src/components/Directives/index.vue
+++ src/components/Directives/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <div v-awesome.red.big="textToRender"></div>
+  <!-- <div v-awesome.red.big="textToRender"></div>
   <button @click="textToRender = 'Updated Text'">Trigger Update</button>
+  -->
+  <hr/>
+  <div v-user-lastname="lastname"></div>
 </template>
 
 <script setup>
   import { ref } from 'vue';
   const textToRender = ref('Some text');
+
+  const lastname = ref('Jones');
+  const vUserLastname = {
+    beforeMount(el, binding) {
+      el.innerHTML = binding.value;
+      el.style.color = 'blue';
+    }
+  }
 </script>
